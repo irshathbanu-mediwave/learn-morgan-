e87a6badef5dbdc8368079813ca3491aa0c5309b
irshathbanu-mediwave <irshathbanu@mindwaveventures.com>
2023-11-03 18:57:59 +0530

    fina

--- app/index.js
+++ app/index.js
@@ -1,104 +1,112 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 import {
-  getAllMovies, findMovieById,
-  addMovie, updateMovie, deleteMovieById
-} from './db.js'
+  getAllMovies,
+  findMovieById,
+  addMovie,
+  updateMovie,
+  deleteMovieById,
+} from "./db.js";
 
 const app = express();
 // allow access from any domain
 app.use(cors());
 // logs
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 // allow parsing of request body
-app.use(express.json())
+app.use(express.json());
 
 // READ
-app.get('/movies', (req, res) => {
-  res.json(getAllMovies())
-})
+app.get("/movies", (req, res) => {
+  res.json(getAllMovies());
+});
 
 // READ
-app.get('/movies/:id', (req, res, next) => {
-  const movie = findMovieById(req.params.id)
+app.get("/movies/:id", (req, res, next) => {
+  const movie = findMovieById(req.params.id);
   if (!movie) {
-    return next({code: 404, message: 'Movie not found for id'})
+    return next({ code: 404, message: "Movie not found for id" });
   }
-  res.json(movie)
-})
+  res.json(movie);
+});
 
 // UPDATE
-app.put('/movies/:id', (req, res, next) => {
+app.put("/movies/:id", (req, res, next) => {
   if (!req.body.title || !req.body.year) {
-    return next({code: 400, message: 'Movie payload should have title and year'})
+    return next({
+      code: 400,
+      message: "Movie payload should have title and year",
+    });
   }
   const m = updateMovie({
     id: req.params.id,
     title: req.body.title,
-    year: req.body.year
-  })
+    year: req.body.year,
+  });
 
   if (!m) {
     return next({
       code: 400,
-      message: 'Failed to update movie with id '+req.params.id
-    })
+      message: "Failed to update movie with id " + req.params.id,
+    });
   }
 
-  return res.json(m)
-})
+  return res.json(m);
+});
 
 // DELETE
-app.delete('/movies/:id', (req, res, next) => {
-  const m = deleteMovieById(req.params.id)
+app.delete("/movies/:id", (req, res, next) => {
+  const m = deleteMovieById(req.params.id);
   if (!m) {
     return next({
       code: 400,
-      message: 'Failed to delete movie with id '+req.params.id
-    })
+      message: "Failed to delete movie with id " + req.params.id,
+    });
   }
-  res.json(m)
-})
+  res.json(m);
+});
 
 // CREATE
-app.post('/movies', (req, res, next) => {
+app.post("/movies", (req, res, next) => {
   if (!req.body.title || !req.body.year) {
-    return next({code: 400, message: 'Movie payload should have title and year'})
+    return next({
+      code: 400,
+      message: "Movie payload should have title and year",
+    });
   }
 
   const newMovie = addMovie({
     title: req.body.title,
-    year: req.body.title,
-  })
+    year: req.body.year,
+  });
 
-  return res.json(newMovie)
-})
-
+  return res.json(newMovie);
+});
 
 // 404
 app.use((req, res, next) => {
   next({
     code: 404,
-    message: 'Not found'
-  })
-})
+    message: "Not found",
+  });
+});
 
 app.use((err, req, res, next) => {
   if (err.code && err.code) {
-    res.status(err.code)
+    res.status(err.code);
   }
   if (err.message && err.message) {
-    res.send({ message: err.message })
+    res.send({ message: err.message });
     return;
   }
 
   return res.status(500).send({
-    message: 'Unknown error'
-  })
-})
+    message: "Unknown error",
+  });
+});
 
 app.listen(5476, () => {
-  console.log('Server running on port 5476');
-})
+  console.log("Server running on port 5476");
+});
